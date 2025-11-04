b83210a36f00a524ac8c5c050e535bccc226237e
wenjingdl <jingwen@cn.ibm.com>
2025-11-04 16:08:44 +0800

    Update SwaggerConfig.java

--- backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
+++ backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig {
                         .url("https://github.com/tianyf925"))
                 )
                 .servers(java.util.Arrays.asList(
-                        new Server().url("http://13.60.85.242:8080"),
+                        new Server().url("http://51.20.75.11:8080"),
                         new Server().url("http://api.airline.com")
                 ))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
